dc5d9f253d66398c165a60fd10b4ac2543a86fc7
Franco-Fantini <franco.fantini.1993@gmail.com>
2024-08-21 13:50:09 +0200

    adding some misc like body_class and meta lang with php wordpress functions

--- app/public/wp-content/themes/UniversityTheme/header.php
+++ app/public/wp-content/themes/UniversityTheme/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes() ?>>
 
 <head>
     <?php wp_head(); ?>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset') ?>">
     <meta name="keywords" content="HTML, CSS, JavaScript, PHP, WordpressCore">
     <meta name="author" content="Franco Fantini">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<body>
+<body <?php body_class() ?>>
     <header class="site-header">
         <div class="container">
             <h2 class="school-logo-text float-left">
--- app/public/wp-content/themes/UniversityTheme/page.php
+++ app/public/wp-content/themes/UniversityTheme/page.php
@@ -3,7 +3,6 @@ get_header();
 
 while (have_posts()) {
     the_post(); ?>
-
     <div class="page-banner">
         <div class="page-banner__bg-image"
             style="background-image:url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
